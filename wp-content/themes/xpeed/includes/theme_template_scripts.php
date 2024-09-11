91ef16128de602bf387c34d01db3ce0d91ad2b7c
uynnph15055 <uynnhuongson123@gmail.com>
2024-09-11 20:56:30 +0700

    save change

--- wp-content/themes/xpeed/includes/theme_template_scripts.php
+++ wp-content/themes/xpeed/includes/theme_template_scripts.php
@@ -4,14 +4,12 @@ if (!function_exists('template_scripts')) {
     {
         // CSS
         wp_enqueue_style('fancybox', 'https://cdnjs.cloudflare.com/ajax/libs/fancyapps-ui/5.0.20/fancybox/fancybox.min.css');
-        wp_enqueue_style('bootstrap-css', 'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/css/toastr.css', array(), '4.1.1');
         wp_enqueue_style('icon-font', 'https://pro.fontawesome.com/releases/v5.15.0/css/all.css', false, '5.15', 'all');
 
         wp_enqueue_style('main-css', DIR_URI . '/assets/css/main.css?r=' . rand(100, 999), array(), THEME_VER);
         wp_enqueue_script('fancybox', 'https://cdnjs.cloudflare.com/ajax/libs/fancyapps-ui/5.0.20/fancybox/fancybox.umd.js', array('jquery'), '5.0.0', true);
 
         wp_enqueue_script('main-js', DIR_URI . '/assets/js/main.js', array(), THEME_VER, true);
-     
     }
 }
 
