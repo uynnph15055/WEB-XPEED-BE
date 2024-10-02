71989940c252b283fa5cc316b16677269cfbc3f3
phuongdd <dangxuong2000@gmail.com>
2024-10-02 23:19:13 +0700

    update regisst

--- wp-content/themes/xpeed/app/Requests/RegisterRequest.php
+++ wp-content/themes/xpeed/app/Requests/RegisterRequest.php
@@ -28,9 +28,9 @@ class RegisterRequest
     public function rules()
     {
         return [
-            'username' => 'required|min:3|max:20|unique:users,username',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|min:6',
+            'username' => 'required|min:3|max:20',
+            'email' => 'required|email',
+            'password' => 'required',
             'address' => 'required',
         ];
     }
